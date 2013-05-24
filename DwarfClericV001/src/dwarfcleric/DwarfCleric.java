b7de1e4aa4b8c77ba506369c604d6aee46432b45
golddiamonds <rhall@berkeley.edu>
2013-05-24 12:26:29 -0700

    Added lwjgl import statements to DwarfCleric class

--- DwarfClericV001/src/dwarfcleric/DwarfCleric.java
+++ DwarfClericV001/src/dwarfcleric/DwarfCleric.java
@@ -1,10 +1,36 @@
 package dwarfcleric;
 
+import static org.lwjgl.opengl.GL11.GL_POLYGON;
+
+import static org.lwjgl.opengl.GL11.glBegin;
+import static org.lwjgl.opengl.GL11.glColor3f;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glFlush;
+import static org.lwjgl.opengl.GL11.glVertex3f;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL11.*;
+import org.lwjgl.util.glu.GLU;
+import org.lwjgl.util.glu.GLU.*;
+
+import java.awt.Font;
+import java.io.InputStream;
+import java.nio.FloatBuffer;
+
+import java.util.*;
+import org.lwjgl.util.glu.Sphere;
+
 public class DwarfCleric {
 
 	
 	public static void main(String[] args) {
 		
+	
 	}
 
 }
