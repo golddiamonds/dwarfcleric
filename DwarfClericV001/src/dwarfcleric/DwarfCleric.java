ad5fde0c133f4211613c658ecf1ffba3675873f9
tavishall <tavis.l.hall@gmail.com>
2013-06-04 22:11:01 -0700

    Messing around a bit with Keyboard, Mouse, Display stuff.

--- DwarfClericV001/src/dwarfcleric/DwarfCleric.java
+++ DwarfClericV001/src/dwarfcleric/DwarfCleric.java
@@ -11,6 +11,7 @@ import static org.lwjgl.opengl.GL11.glVertex3f;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
@@ -27,10 +28,61 @@ import org.lwjgl.util.glu.Sphere;
 
 public class DwarfCleric {
 
+	public void startDisplay() {
+		// This code came from lwjgl tutorial 1 (Display)
+		try {
+			Display.setDisplayMode(new DisplayMode(800,600));
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		// initialize OpenGL here
+		while (!Display.isCloseRequested()) {
+			// render OpenGL here
+			
+			pollInput(); //I guess I don't need a this.pollInput() ?
+			Display.update();
+		}
+		
+		Display.destroy();
+	}
+	
+	
+	public void pollInput() {
+		//Check the mouse button down
+//		if (Mouse.isButtonDown(0)) {
+//			int x = Mouse.getX();
+//			int y = Mouse.getY();
+//			
+//			System.out.println("Mouse was left clicked at X: " + x + " Y: " + y);
+//		}
+		
+		//Doing it this way ensures that we only get one event per click
+		while (Mouse.next()) {
+			if (Mouse.getEventButtonState()) {
+				if (Mouse.getEventButton() == 0) {
+					System.out.println("CLICKED: LEFT");
+				}
+			}
+		}
+		
+		//Check the keyboard
+		while (Keyboard.next()) {
+			if (Keyboard.getEventKeyState()) {
+				if (Keyboard.getEventKey() == Keyboard.KEY_A) {
+					System.out.println("PRESSED: A");
+				}
+			}
+		}
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
-	
+		
+		DwarfCleric dc = new DwarfCleric();
+		dc.startDisplay();
 	}
 
 }
