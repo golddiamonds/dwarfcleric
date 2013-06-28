c83771f71f9e00a1c4ab7ddbf0e7947e88727654
golddiamonds <rhall@berkeley.edu>
2013-06-28 10:17:19 -0700

    Removed DwarfClericV001 directory.

---
+++ src/dwarfcleric/Camera.java
@@ -0,0 +1,18 @@
+package dwarfcleric;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.vector.Vector3f;
+
+public class Camera {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
